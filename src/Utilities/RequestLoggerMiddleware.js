67f1a87be2c9bd49e30ce5e06636fcdf21aec1b5
AbutalhaKothiwale53 <talhakothiwale53@gmail.com>
2024-01-30 00:28:44 +0530

    v0.0.01
    Added Backend

--- src/Utilities/RequestLoggerMiddleware.js
+++ src/Utilities/RequestLoggerMiddleware.js
@@ -1,16 +1,21 @@
 const fs = require("fs");
 const { promisify } = require("util");
 const appendFile = promisify(fs.appendFile);
-const path = require('path')
+const path = require("path");
+
 async function RequestLogger(req, res, next) {
-    try {
-        const logsDirectory = './Logs';
-        const logFilePath = path.join(logsDirectory, "ApiLoggingLogs.log")
-        logMessage = `${new Date()} - ${req.method} - ${req.url} - ${req.body} \n`;
-        await appendFile(logFilePath, logMessage);
-        next();
-    } catch (err) {
-        next(err);
+  try {
+    const logsDirectory = "./Logs";
+    const logFilePath = path.join(logsDirectory, "ApiLoggingLogs.log");
+    // Ensure the 'Logs' directory exists
+    if (!fs.existsSync(logsDirectory)) {
+      fs.mkdirSync(logsDirectory);
     }
+    logMessage = `${new Date()} - ${req.method} - ${req.url} - ${req.body} \n`;
+    await appendFile(logFilePath, logMessage);
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
 module.exports = RequestLogger;
